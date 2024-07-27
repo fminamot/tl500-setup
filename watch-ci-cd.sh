d9a55d9d266970ec458b05376460dad06e72eabb
Fusayuki Minamoto <user1@redhatlabs.dev>
2024-07-27 14:40:58 +0900

    fix

--- watch-ci-cd.sh
+++ watch-ci-cd.sh
@@ -2,8 +2,5 @@
 
 source checkvar
 
-oc get deploy -n ${TEAM_NAME}-ci-cd
-oc get pods -n ${TEAM_NAME}-ci-cd
-
-
+watch -n 10 'oc get deploy -n ${TEAM_NAME}-ci-cd; oc get pods -n ${TEAM_NAME}-ci-cd'
 
