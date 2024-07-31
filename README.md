dd80d11e2105c19e83cc39469e36192c0011544e
Fusayuki Minamoto <user1@redhatlabs.dev>
2024-07-31 13:00:09 +0900

    fix

--- README.md
+++ README.md
@@ -5,6 +5,4 @@
   <dd>HTML版の技術演習テキストを読むためのコンテナーアプリケーション</dd>
   <dt>scripts</dt>
   <dd>TL500ラボ環境の準備に使うシェルスクリプト</dd>
-   <dt>textbook</dt>
-  <dd>PDF版の技術演習テキスト</dd>
 </dl>
--- scripts/show-consoles.sh
+++ scripts/show-consoles.sh
@@ -4,27 +4,27 @@ source checkvar
 
 echo "**** Team: ${TEAM_NAME} ****"
 
-echo "OpenShift Web Console"
+echo "\nOpenShift Web Console"
 echo https://console-openshift-console.apps.ocp4.example.com
 
-echo "GitLab Server"
+echo "\nGitLab Server"
 echo "https://gitlab-ce.apps.ocp4.example.com"
 
-echo "ArgoCD UI"
+echo "\nArgoCD UI"
 echo "https://argocd-server-${TEAM_NAME}-ci-cd.apps.ocp4.example.com"
 
-echo "tech-exercise WebHook"
+echo "\ntech-exercise WebHook"
 echo https://argocd-server-${TEAM_NAME}-ci-cd.apps.ocp4.example.com/api/webhook
 
-pecho "pet-battle-api WebHook"
+echo "\npet-battle-api WebHook"
 echo "https://webhook-${TEAM_NAME}-ci-cd.apps.ocp4.example.com"
 
-echo "Nexus UI"
+echo "\nNexus UI"
 echo "https://nexus-${TEAM_NAME}-ci-cd.apps.ocp4.example.com"
 
-echo "SonarQube UI"
+echo "\nSonarQube UI"
 echo "https://sonarqube-${TEAM_NAME}-ci-cd.apps.ocp4.example.com"
 echo "username: admin, password: admin123"
 
-echo "Allure UI"
+echo "\nAllure UI"
 echo "https://allure-${TEAM_NAME}-ci-cd.apps.ocp4.example.com/allure-docker-service/projects/pet-battle-api/reports/latest/index.html"
