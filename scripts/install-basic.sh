6e558cde945296c7ce9079ea36e006e781d8049f
Fusayuki Minamoto <user1@redhatlabs.dev>
2024-08-22 11:06:53 +0900

    Refactoring functions

--- scripts/install-basic.sh
+++ scripts/install-basic.sh
@@ -2,8 +2,6 @@
 
 source basic
 
-echo "********** GitLab PAT"
-
 gitlab_pat () {
         [ -z "$GIT_SERVER" ] && echo "Warning: must supply GIT_SERVER in env" && return
         [ -z "$GITLAB_USER" ] && echo "Warning: must supply GITLAB_USER in env" && return
@@ -28,40 +26,55 @@ gitlab_pat () {
                         --data 'personal_access_token[name]='"${GITLAB_USER}"'&personal_access_token[expires_at]=&personal_access_token[scopes][]=api') 
         GITLAB_PAT=$(echo $body_header | perl -ne 'print "$1\n" if /created-personal-access-token"[[:blank:]]value="(.+?)"/' | sed -n 1p) 
         echo $GITLAB_PAT
+        export GITLAB_PAT
 }
 
+print_vars () {
+        echo "TEAM_NAME=$TEAM_NAME"
+        echo "USER_NAME=$USER_NAME"
+        echo "PASSWORD=$PASSWORD"
+        echo "CLUSTER_DOMAIN=$CLUSTER_DOMAIN"
+        echo "GIT_SERVER=$GIT_SERVER"
+        echo "GITLAB_USER=$GITLAB_USER"
+        echo "GITLAB_PAT=$GITLAB_PAT"
+}
+
+export_vars () {
+        echo export TEAM_NAME=$TEAM_NAME | tee -a ~/.bashrc -a ~/.zshrc
+        echo export USER_NAME=$USER_NAME | tee -a ~/.bashrc -a ~/.zshrc
+        echo export PASSWORD=$PASSWORD | tee -a ~/.bashrc -a ~/.zshrc
+        echo export CLUSTER_DOMAIN=$CLUSTER_DOMAIN | tee -a ~/.bashrc -a ~/.zshrc
+        echo export GIT_SERVER=$GIT_SERVER | tee -a ~/.bashrc -a ~/.zshrc
+        echo export GITLAB_USER=$GITLAB_USER | tee -a ~/.bashrc -a ~/.zshrc
+        echo export GITLAB_PAT=$GITLAB_PAT | tee -a ~/.bashrc -a ~/.zshrc
+
+}
+
+create_repos () {
+        # Group作成
+        curl -k -X POST \
+        -H "PRIVATE-TOKEN: ${GITLAB_PAT}" -H "Content-Type:application/json" \
+        "https://${GIT_SERVER}/api/v4/groups/" -d "{\"path\": \"${TEAM_NAME}\", \"name\": \"${TEAM_NAME}\", \"visibility\": \"public\"}"
+
+        # Namespace ID取得
+        NAMESPACE_ID=$(curl -s -k -H "PRIVATE-TOKEN: ${GITLAB_PAT}" "https://${GIT_SERVER}/api/v4/namespaces/${TEAM_NAME}" | jq -r ".id")
+        echo $NAMESPACE_ID
+
+        # Git Repository作成
+        for REPO_NAME in tech-exercise pet-battle-api
+        do
+        curl -k -X POST \
+        -H "PRIVATE-TOKEN: ${GITLAB_PAT}" -H "Content-Type:application/json" \
+        "https://${GIT_SERVER}/api/v4/projects/" -d "{\"namespace_id\": \"${NAMESPACE_ID}\", \"name\": \"${REPO_NAME}\",\"visibility\": \"internal\"}"
+        done
+
+}
+
+
 gitlab_pat
-
-export GITLAB_PAT
-
-echo "********** Basic Settings"
-
-echo export TEAM_NAME=$TEAM_NAME | tee -a ~/.bashrc -a ~/.zshrc
-echo export USER_NAME=$USER_NAME | tee -a ~/.bashrc -a ~/.zshrc
-echo export PASSWORD=$PASSWORD | tee -a ~/.bashrc -a ~/.zshrc
-echo export CLUSTER_DOMAIN=$CLUSTER_DOMAIN | tee -a ~/.bashrc -a ~/.zshrc
-echo export GIT_SERVER=$GIT_SERVER | tee -a ~/.bashrc -a ~/.zshrc
-echo export GITLAB_USER=$GITLAB_USER | tee -a ~/.bashrc -a ~/.zshrc
-echo export GITLAB_PAT=$GITLAB_PAT | tee -a ~/.bashrc -a ~/.zshrc
-
-# Group作成
-curl -k -X POST \
-  -H "PRIVATE-TOKEN: ${GITLAB_PAT}" -H "Content-Type:application/json" \
-  "https://${GIT_SERVER}/api/v4/groups/" -d "{\"path\": \"${TEAM_NAME}\", \"name\": \"${TEAM_NAME}\", \"visibility\": \"public\"}"
-
-# Namespace ID取得
-NAMESPACE_ID=$(curl -s -k -H "PRIVATE-TOKEN: ${GITLAB_PAT}" "https://${GIT_SERVER}/api/v4/namespaces/${TEAM_NAME}" | jq -r ".id")
-echo $NAMESPACE_ID
-
-# Git Repository作成
-for REPO_NAME in tech-exercise pet-battle-api
-do
-curl -k -X POST \
-  -H "PRIVATE-TOKEN: ${GITLAB_PAT}" -H "Content-Type:application/json" \
-  "https://${GIT_SERVER}/api/v4/projects/" -d "{\"namespace_id\": \"${NAMESPACE_ID}\", \"name\": \"${REPO_NAME}\",\"visibility\": \"internal\"}"
-done
-  
-./printbasic.sh
+export_vars
+create_repos
+print_vars
 
 echo "\ninstall-basic done"
 
