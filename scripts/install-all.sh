673f8ed41f828f635fa48ef9677b5984c67795a2
Fusayuki Minamoto <user1@redhatlabs.dev>
2024-08-22 11:42:18 +0900

    Refactoring gitlab_pat

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-source ./basic
+source basic
 ./install-basic.sh
 ./install-uj.sh
 ./install-uj2.sh
 ./install-tekton.sh
 ./install-sonarqube.sh
 ./install-allure.sh
-echo "\ninstall-all done"
-
-
-
+echo "install-all done"
