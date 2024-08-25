0e9cf2cc0e21ad39a92695f8fa1c2ed382796f66
Fusayuki Minamoto <user1@redhatlabs.dev>
2024-08-25 12:17:14 +0900

    Added -e option to echo

--- scripts/show-consoles.sh
+++ scripts/show-consoles.sh
@@ -4,27 +4,27 @@ source checkvar
 
 echo "**** Team: ${TEAM_NAME} ****"
 
-echo "\nOpenShift Web Console"
+echo -e "\nOpenShift Web Console"
 echo https://console-openshift-console.apps.ocp4.example.com
 
-echo "\nGitLab Server"
+echo -e "\nGitLab Server"
 echo "https://gitlab-ce.apps.ocp4.example.com"
 
-echo "\nArgoCD UI"
+echo -e "\nArgoCD UI"
 echo "https://argocd-server-${TEAM_NAME}-ci-cd.apps.ocp4.example.com"
 
-echo "\ntech-exercise WebHook"
+echo -e "\ntech-exercise WebHook"
 echo https://argocd-server-${TEAM_NAME}-ci-cd.apps.ocp4.example.com/api/webhook
 
-echo "\npet-battle-api WebHook"
+echo -e "\npet-battle-api WebHook"
 echo "https://webhook-${TEAM_NAME}-ci-cd.apps.ocp4.example.com"
 
-echo "\nNexus UI"
+echo -e "\nNexus UI"
 echo "https://nexus-${TEAM_NAME}-ci-cd.apps.ocp4.example.com"
 
-echo "\nSonarQube UI"
+echo -e "\nSonarQube UI"
 echo "https://sonarqube-${TEAM_NAME}-ci-cd.apps.ocp4.example.com"
 echo "username: admin, password: admin123"
 
-echo "\nAllure UI"
+echo -e "\nAllure UI"
 echo "https://allure-${TEAM_NAME}-ci-cd.apps.ocp4.example.com/allure-docker-service/projects/pet-battle-api/reports/latest/index.html"
