7a376c8a1ec2f5364c0d52d2de9db0b3a7984dab
Fusayuki Minamoto <user1@redhatlabs.dev>
2024-08-25 10:44:24 +0900

    Show tool versions

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -7,15 +7,18 @@ wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.10/opens
 tar xvf openshift*.tar.gz
 chmod a+x oc
 sudo mv ./oc /usr/local/bin
+oc version
 
 # Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 sudo ./get_helm.sh
+helm --version
 
 # Yq
 sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq 
 sudo chmod +x /usr/local/bin/yq
+yq --version
 
 # Maven
 cd ~
@@ -23,6 +26,7 @@ sudo dnf install java-17-openjdk-devel -y
 wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
 tar xvfz apache-maven-3.9.9-bin.tar.gz
 export PATH=$PATH:/home/student/apache-maven-3.9.9/bin
+mvn --version
 
 # Tech Exercise
 git config --global user.name "student"
